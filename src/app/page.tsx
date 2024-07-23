8419f7ebc8b369a18625eea1509c9579d5c99d17
김덕기 <ejrrlabc@gmail.com>
2024-07-24 01:36:36 +0900

    feat: config 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
 'use client';
 
+import { useEffect } from 'react';
+
 export default function Home() {
-  console.log(process.env.NODE_ENV);
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/sw.js')
+        .then((registration) => {
+          console.log(
+            'ServiceWorker registration successful with scope: ',
+            registration.scope
+          );
+        })
+        .catch((error) => {
+          console.log('ServiceWorker registration failed: ', error);
+        });
+    }
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       ㅋㅋㅋㅋㅋㅋㅋㅋㅋ
