270eeebab663dd5777f095ea55b33041f416e6a9
김덕기 <ejrrlabc@gmail.com>
2024-07-26 18:54:53 +0900

    feat: cache 관련 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,35 +1,4 @@
-import fs from 'fs';
-import path from 'path';
 import withSerwistInit from '@serwist/next';
-
-function getPages(dir, basePath = '', pages = []) {
-  fs.readdirSync(dir).forEach((file) => {
-    const fullPath = path.join(dir, file);
-    if (fs.lstatSync(fullPath).isDirectory()) {
-      getPages(fullPath, path.join(basePath, file), pages);
-    } else if (
-      (file.endsWith('.js') ||
-        file.endsWith('.jsx') ||
-        file.endsWith('.ts') ||
-        file.endsWith('.tsx')) &&
-      !file.includes('layout')
-    ) {
-      let pagePath = path.join(
-        basePath,
-        file.replace(/\.(js|jsx|ts|tsx)$/, '')
-      );
-      if (pagePath.endsWith('/page')) {
-        pagePath = pagePath.replace('/page', '');
-      }
-      pages.push(pagePath === '/index' ? '/' : pagePath);
-    }
-  });
-  return pages;
-}
-
-const pages = getPages(path.join(process.cwd(), 'src/app'));
-
-console.log(pages);
 
 const withSerwist = withSerwistInit({
   // Note: This is only an example. If you use Pages Router,
@@ -38,7 +7,7 @@ const withSerwist = withSerwistInit({
   swDest: 'public/sw.js',
   cacheOnNavigation: true,
   reloadOnOnline: true,
-  additionalPrecacheEntries: pages,
+  additionalPrecacheEntries: ['/test2', '/test1', '/'],
 });
 
 export default withSerwist({
