de6ca2f833bebaad2fb8cebe061b5bba654d63d9
김덕기 <ejrrlabc@gmail.com>
2024-08-01 01:10:39 +0900

    feat: 페이지 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -88,12 +88,6 @@ export default function Home() {
           >
             Page4
           </Link>
-          <Link
-            href="/data-download"
-            className="bg-amber-500 hover:bg-amber-700 text-white font-bold py-2 px-4 rounded"
-          >
-            Data-Download
-          </Link>
         </div>
       </div>
       <button
